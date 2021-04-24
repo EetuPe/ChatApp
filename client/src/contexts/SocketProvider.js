5d10e355df2142d467dee7bc95c3013720c91b15
EetuPe <eetu.petanen@gmail.com>
2021-04-24 16:52:46 +0300

    Add Socket.io and chat functionality

---
+++ client/src/contexts/SocketProvider.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from 'react'
+import io from 'socket.io-client'
+
+const socketContext = React.createContext()
+
+export function useSocket() {
+    return useContext(socketContext)
+}
+
+export default function SocketProvider({ id, children }) {
+    const [socket, setSocket] = useState()
+    
+    useEffect(() => {
+        const newSocket = io(
+            'http://localhost:5000', 
+            { query: { id } }
+        )
+        setSocket(newSocket)
+
+        return () => newSocket.close()
+    }, [id])
+
+    return (
+        <socketContext.Provider value={socket}>
+            {children}
+        </socketContext.Provider>
+    )
+}
